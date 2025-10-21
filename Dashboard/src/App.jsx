7c423e6ec8ec42e5453942c3c31c95dbb637cfd3
Sneha Sharma <snehasharma022005@gmail.com>
2025-10-21 19:40:41 +0530

    Dashboard UI

---
+++ Dashboard/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CommandCenter from "./component/CommandCenter.jsx";
+
+function App() {
+  return (
+    <div style={{ height: "100vh", width: "100%" }}>
+      <CommandCenter />
+    </div>
+  );
+}
+
+export default App;
